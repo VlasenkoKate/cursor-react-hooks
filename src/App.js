d4e50d89a0389d4ed094dd5cf7636f7e705e1901
VlasenkoKate <85071551+VlasenkoKate@users.noreply.github.com>
2021-08-05 23:52:39 +0300

    HW done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Contacts from "/Users/ekaterinavlasenko/Desktop/vjgv/cursor-homework/CURSOR_ADVANCED/18homework/cursor-react-hooks/src/components/Contacts/contacts.js"
+
+function App() {
+  return (
+    <div >
+<Contacts />
+    </div>
+  );
+}
+
+export default App;
